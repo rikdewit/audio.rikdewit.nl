a5a71ffae1c7b2289f970a34c1ef4e2ad2728e63
Rik de Wit <rikdewit1997@gmail.com>
2026-03-11 23:56:54 +0100

    Add cache headers for static images

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ const nextConfig = {
   trailingSlash: true,
   // Ensure the base path is empty for custom domains
   basePath: '',
+  headers: async () => [
+    {
+      source: '/projects/:slug/images/:image*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable'
+        }
+      ]
+    }
+  ]
 };
 
 export default nextConfig;
